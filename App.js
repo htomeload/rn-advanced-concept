77b9092babedbee9901ea42b3a44b467bc59ae62
htomeload <ko_player_555@outlook.com>
2023-11-30 19:35:28 +0700

    Prepare Deck component

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
-import Ball from "./src/ball";
+import { StyleSheet, Text } from "react-native";
 import SafeAreaView from "./src/components/safe-area-view/SafeAreaView";
+import Deck from "./src/Deck";
+
+const DATA = [
+  {
+    id: 1,
+    text: "Card #1",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+  },
+  {
+    id: 2,
+    text: "Card #2",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+  },
+  {
+    id: 3,
+    text: "Card #3",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+  {
+    id: 4,
+    text: "Card #4",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+  },
+  {
+    id: 5,
+    text: "Card #5",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+  },
+  {
+    id: 6,
+    text: "Card #6",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+  },
+  {
+    id: 7,
+    text: "Card #7",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+  {
+    id: 8,
+    text: "Card #8",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+  },
+];
 
 export default function App() {
+  const renderCard = (item) => {
+    return <Text>{item?.text}</Text>;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Ball />
-      <StatusBar style="auto" />
+      <Deck renderCard={renderCard} data={DATA} />
     </SafeAreaView>
   );
 }
---
+++ src/Deck.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, Animated } from "react-native";
+
+export default function Deck({ renderCard, data }) {
+  return <View />;
+}
