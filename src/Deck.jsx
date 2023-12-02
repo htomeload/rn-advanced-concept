23afb283c24194df2cc480c51c1b7b26621ca1b5
htomeload <ko_player_555@outlook.com>
2023-12-02 18:11:14 +0700

    Prepare PanResponder

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { View, Animated, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Animated, StyleSheet, PanResponder } from "react-native";
 
 export default function Deck({ renderCard, data }) {
+  const [panResponder, setPanResponder] = useState(null);
+
   const renderCards = () => {
     return data?.map((item) => renderCard(item));
   };
+
+  useEffect(() => {
+    setPanResponder(
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gesture) => {
+          console.log("gesture: ", gesture);
+        },
+        onPanResponderRelease: () => {},
+      })
+    );
+  }, []);
 
   return <View style={styles.mainContainer}>{renderCards()}</View>;
 }
