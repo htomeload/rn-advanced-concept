ddff3b05db07ea15094612f8047246e3af52c436
htomeload <ko_player_555@outlook.com>
2023-12-05 17:34:40 +0700

    Change logic to animation - Convert position from constant to state - Add reset position with animation

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   View,
   Animated,
   StyleSheet,
   PanResponder,
   FlatList,
+  Dimensions,
 } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function Deck({ renderCard, data }) {
   const [panResponder, setPanResponder] = useState(null);
-  const [axisX, setAxisX] = useState(null);
-  const [axisY, setAxisY] = useState(null);
-
-  const position = new Animated.ValueXY();
-
-  const updatePosition = (gesture) => {
-    setAxisX(gesture?.dx);
-    setAxisY(gesture?.dy);
-  };
+  const [position, setPosition] = useState(new Animated.ValueXY());
 
   const getCardStyle = () => {
     const rotate = position.x.interpolate({
-      inputRange: [-500, 0, 500],
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
       outputRange: ["-120deg", "0deg", "120deg"],
     });
 
@@ -31,21 +26,30 @@ export default function Deck({ renderCard, data }) {
     };
   };
 
+  const backToDefaultPosition = () => {
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
+    }).start();
+  };
+
   useEffect(() => {
     setPanResponder(
       PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderMove: (event, gesture) => {
-          updatePosition?.(gesture);
+          position.setValue({ x: gesture.dx, y: gesture.dy });
         },
-        onPanResponderRelease: () => {},
+        onPanResponderRelease: () => {
+          backToDefaultPosition();
+        },
       })
     );
   }, []);
 
   useEffect(() => {
-    position.setValue({ x: axisX, y: axisY });
-  }, [axisX, axisY]);
+    setPosition(position);
+  }, [position]);
 
   if (!panResponder) return null;
 
